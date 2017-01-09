b799d8e152df694e0897eb36661708e344d9eac4
Rishabh Ravindra <rishabhravindra@gmail.com>
2017-01-09 08:19:22 -0500

    Add new card for timeDown

--- js/time.js
+++ js/time.js
@@ -2,10 +2,11 @@ var time = 0;
 var running = 0;
 
 function initializeClock() {
-	var countTarget = document.getElementById("inputTime").value;
+	var countTarget = document.getElementById("inputTime");
 	if(running == 0) {
 		running = 1;
 		increment();
+		decrement();
 		document.getElementById("play").className = "fa fa-pause";
 	}
 	return countTarget;
@@ -34,15 +35,45 @@ function reset() {
 }
 function increment () {
 	if(running == 1) {
-		target = initializeClock();
+		targetUp = initializeClock();
 		setTimeout(function(){
-			if(target >= time) {
+			if(targetUp.value >= time ) {
 			var days  =  Math.floor(time / 86400);
 			var hours =  Math.floor((time % 86400) / 3600);
 			var mins = Math.floor(((time % 86400) % 3600) / 60);
 			var secs =  Math.floor(((time % 86400) % 3600) % 60);;
 			
 			time++;
+
+			if (days < 10) {days = "0" + days;}
+			if (hours < 10) {hours = "0" + hours;}
+			if (mins < 10) {mins = "0" + mins;}
+			if (secs < 10) {secs = "0" + secs;}
+
+			document.getElementById("getDays").innerHTML = days;
+			document.getElementById("getHr").innerHTML = hours;
+			document.getElementById("getMin").innerHTML = mins;
+			document.getElementById("getSec").innerHTML = secs;
+			increment(); }
+			else {
+				running = 0;
+				time = 0;
+				document.getElementById("play").className = "fa fa-play";
+			}
+		}, 1000);
+	}
+}
+function decrement () {
+	if(running == 1) {
+		targetDown = initializeClock();
+		setTimeout(function(){
+			if(targetDown.value <= time ) {
+			var days  =  Math.floor(time / 86400);
+			var hours =  Math.floor((time % 86400) / 3600);
+			var mins = Math.floor(((time % 86400) % 3600) / 60);
+			var secs =  Math.floor(((time % 86400) % 3600) % 60);;
+			
+			timeDown--;
 
 			if (days < 10) {days = "0" + days;}
 			if (hours < 10) {hours = "0" + hours;}
