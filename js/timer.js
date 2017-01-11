7612d63b93aa89fb9baab782175e54d54c449b28
Rishabh Ravindra <rishabhravindra@gmail.com>
2017-01-11 17:01:33 -0500

    Spacing between cards

--- js/timer.js
+++ js/timer.js
@@ -6,150 +6,165 @@ var resetDownId;
 var resetUpId;
 var remainingTime;
 
+document.getElementById("startPauseUp").disabled = true;
+document.getElementById("startPauseDown").disabled = true;
+
 function getTarget() {
-		var countTarget = document.getElementById("inputTime").value;
-		return countTarget;
+    var countTarget = document.getElementById("inputTime").value;
+    return countTarget;
 }
 
 var countUpIcon = document.getElementById("playUp");
 var countDownIcon = document.getElementById("playDown");
+
 function initializeClock() {
-				
-
-	if(runningUp == 0) {
-		runningUp = 1;
-		increment();
-		 countUpIcon.className = "fa fa-pause";
-		document.getElementById("startPauseUp").disabled = false;
-	}
-	else {
-		runningUp = 0;
-		countUpIcon.className = "fa fa-play";
-	}
-	// startPauseDown();
-	if(runningDown == 0) {
-		runningDown = 1;
-		console.log('TImedown outside decrement ' + timeDown);
-		decrement(getTarget());
-		countDownIcon.className = "fa fa-pause";
-		document.getElementById("startPauseDown").disabled = false;
-	}
-	else {
-		runningDown = 0;
-		countDownIcon.className = "fa fa-play";
-	}	
+    if (runningUp == 0) {
+        runningUp = 1;
+        increment(getTarget());
+        countUpIcon.className = "fa fa-pause";
+        document.getElementById("startPauseUp").disabled = false;
+    } else {
+        runningUp = 0;
+        countUpIcon.className = "fa fa-play";
+    }
+    // startPauseDown();
+    if (runningDown == 0) {
+        runningDown = 1;
+        console.log('TImedown outside decrement ' + timeDown);
+        decrement(getTarget());
+        countDownIcon.className = "fa fa-pause";
+        document.getElementById("startPauseDown").disabled = false;
+    } else {
+        runningDown = 0;
+        countDownIcon.className = "fa fa-play";
+    }
 }
+
 function startPauseUp() {
-	if(runningUp == 0) {
-		runningUp = 1;
-		increment();
-		countUpIcon.className = "fa fa-pause";
-	}
-	else {
-		runningUp = 0;
-		countUpIcon.className = "fa fa-play";
-	}
+    if (runningUp == 0) {
+        runningUp = 1;
+        increment();
+        countUpIcon.className = "fa fa-pause";
+    } else {
+        runningUp = 0;
+        countUpIcon.className = "fa fa-play";
+    }
 }
+
 function startPauseDown() {
-	if(runningDown == 0) {
-		runningDown = 1;
-		console.log('TImedown outside decrement ' + timeDown);
-		decrement(remainingTime);
-		countDownIcon.className =  "fa fa-pause";
-	}
-	else {
-		runningDown = 0;
-		countDownIcon.className =  "fa fa-play";
-	}
+    if (runningDown == 0) {
+        runningDown = 1;
+        console.log('TImedown outside decrement ' + timeDown);
+        decrement(remainingTime);
+        countDownIcon.className = "fa fa-pause";
+    } else {
+        runningDown = 0;
+        countDownIcon.className = "fa fa-play";
+    }
 }
+
 function resetUp() {
-	clearTimeout(resetUpId);
-	runningUp = 0;
-	timeUp = 0;
-	document.getElementById("startPauseUp").disabled = true;
-	document.getElementById("getDaysUp").innerHTML = "00";
-	document.getElementById("getHrUp").innerHTML = "00";
-	document.getElementById("getMinUp").innerHTML = "00";
-	document.getElementById("getSecUp").innerHTML = "00";
-	countUpIcon.className = "fa fa-play";
-
-
-	
-
+    clearTimeout(resetUpId);
+    runningUp = 0;
+    timeUp = 0;
+    document.getElementById("startPauseUp").disabled = true;
+    document.getElementById("getDaysUp").innerHTML = "00";
+    document.getElementById("getHrUp").innerHTML = "00";
+    document.getElementById("getMinUp").innerHTML = "00";
+    document.getElementById("getSecUp").innerHTML = "00";
+    countUpIcon.className = "fa fa-play";
 }
+
 function resetDown() {
-	clearTimeout(resetDownId);
-	runningDown = 0;
-	timeDown = 0;
-	document.getElementById("startPauseDown").disabled = true;
-	document.getElementById("getDaysDown").innerHTML = "00";
-	document.getElementById("getHrDown").innerHTML = "00";
-	document.getElementById("getMinDown").innerHTML = "00";
-	document.getElementById("getSecDown").innerHTML = "00";
-	countDownIcon.className =  "fa fa-play";
+    clearTimeout(resetDownId);
+    runningDown = 0;
+    timeDown = 0;
+    document.getElementById("startPauseDown").disabled = true;
+    document.getElementById("getDaysDown").innerHTML = "00";
+    document.getElementById("getHrDown").innerHTML = "00";
+    document.getElementById("getMinDown").innerHTML = "00";
+    document.getElementById("getSecDown").innerHTML = "00";
+    countDownIcon.className = "fa fa-play";
 }
-function increment () {
-	if(runningUp == 1) {
-		targetUp = getTarget();
-		var resetUpId = setTimeout(function(){
-			if(targetUp >= timeUp ) {
-			var days  =  Math.floor(timeUp / 86400);
-			var hours =  Math.floor((timeUp % 86400) / 3600);
-			var mins = Math.floor(((timeUp % 86400) % 3600) / 60);
-			var secs =  Math.floor(((timeUp % 86400) % 3600) % 60);;
-			
-			timeUp++;
 
-			if (days < 10) {days = "0" + days;}
-			if (hours < 10) {hours = "0" + hours;}
-			if (mins < 10) {mins = "0" + mins;}
-			if (secs < 10) {secs = "0" + secs;}
+function increment() {
+    if (runningUp == 1) {
+        targetUp = getTarget();
+        var resetUpId = setTimeout(function() {
+            if (targetUp >= timeUp) {
+                var days = Math.floor(timeUp / 86400);
+                var hours = Math.floor((timeUp % 86400) / 3600);
+                var mins = Math.floor(((timeUp % 86400) % 3600) / 60);
+                var secs = Math.floor(((timeUp % 86400) % 3600) % 60);;
 
-			document.getElementById("getDaysUp").innerHTML = days;
-			document.getElementById("getHrUp").innerHTML = hours;
-			document.getElementById("getMinUp").innerHTML = mins;
-			document.getElementById("getSecUp").innerHTML = secs;
-			increment(); }
-			else {
-				runningUp = 0;
-				timeUp = 0;
-					countUpIcon.className =  "fa fa-play";
-				document.getElementById("startPauseDown").disabled = true;
-			}
-		}, 100);
-	}
+                timeUp++;
+
+                if (days < 10) {
+                    days = "0" + days;
+                }
+                if (hours < 10) {
+                    hours = "0" + hours;
+                }
+                if (mins < 10) {
+                    mins = "0" + mins;
+                }
+                if (secs < 10) {
+                    secs = "0" + secs;
+                }
+
+                document.getElementById("getDaysUp").innerHTML = days;
+                document.getElementById("getHrUp").innerHTML = hours;
+                document.getElementById("getMinUp").innerHTML = mins;
+                document.getElementById("getSecUp").innerHTML = secs;
+                increment();
+            } else {
+                runningUp = 0;
+                timeUp = 0;
+                countUpIcon.className = "fa fa-play";
+                document.getElementById("startPauseDown").disabled = true;
+            }
+        }, 1000);
+    }
 }
 var count = 0;
-function decrement (timeDown) {
-	if(runningDown == 1) {
-		resetDownId = setTimeout(function(){ 
-			if(timeDown >= 0 ) {
-			var days  =  Math.floor(timeDown / 86400);
-			var hours =  Math.floor((timeDown % 86400) / 3600);
-			var mins = Math.floor(((timeDown % 86400) % 3600) / 60);
-			var secs =  Math.floor(((timeDown % 86400) % 3600) % 60);
-			count++;
 
-			timeDown--;
-			remainingTime = timeDown;
-			console.log('TImedown inside decrement ' + timeDown);
-			if (days < 10) {days = "0" + days;}
-			if (hours < 10) {hours = "0" + hours;}
-			if (mins < 10) {mins = "0" + mins;}
-			if (secs < 10) {secs = "0" + secs;}
+function decrement(timeDown) {
+    if (runningDown == 1) {
+        resetDownId = setTimeout(function() {
+            if (timeDown >= 0) {
+                var days = Math.floor(timeDown / 86400);
+                var hours = Math.floor((timeDown % 86400) / 3600);
+                var mins = Math.floor(((timeDown % 86400) % 3600) / 60);
+                var secs = Math.floor(((timeDown % 86400) % 3600) % 60);
+                count++;
 
-			document.getElementById("getDaysDown").innerHTML = days;
-			document.getElementById("getHrDown").innerHTML = hours;
-			document.getElementById("getMinDown").innerHTML = mins;
-			document.getElementById("getSecDown").innerHTML = secs;
-			decrement(timeDown); }
-			else {
-				runningDown = 0;
-				timeDown = 0;
-				countDownIcon.className = "fa fa-play";
-				document.getElementById("startPauseUp").disabled = true;
-			}
-		}, 100);
-	}
-}	
+                timeDown--;
+                remainingTime = timeDown;
+                console.log('TImedown inside decrement ' + timeDown);
+                if (days < 10) {
+                    days = "0" + days;
+                }
+                if (hours < 10) {
+                    hours = "0" + hours;
+                }
+                if (mins < 10) {
+                    mins = "0" + mins;
+                }
+                if (secs < 10) {
+                    secs = "0" + secs;
+                }
 
+                document.getElementById("getDaysDown").innerHTML = days;
+                document.getElementById("getHrDown").innerHTML = hours;
+                document.getElementById("getMinDown").innerHTML = mins;
+                document.getElementById("getSecDown").innerHTML = secs;
+                decrement(timeDown);
+            } else {
+                runningDown = 0;
+                timeDown = 0;
+                countDownIcon.className = "fa fa-play";
+                document.getElementById("startPauseUp").disabled = true;
+            }
+        }, 1000);
+    }
+}
