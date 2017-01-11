4aa0ee393ddf6073410dbe4c94e028ec095e7352
Rishabh Ravindra <rishabhravindra@gmail.com>
2017-01-11 02:31:28 -0500

    add clearTimeout function

--- js/timer.js
+++ js/timer.js
@@ -2,21 +2,14 @@ var timeUp = 0;
 var runningUp = 0;
 var timeDown = getTarget();
 var runningDown = 0;
+var resetDownId;
 function getTarget() {
 		var countTarget = document.getElementById("inputTime").value;
 		return countTarget;
 }
 function initializeClock() {
-	if(runningUp == 0) {
-		runningUp = 1;
-		increment();
-		document.getElementById("playUp").className = "fa fa-pause";
-	}
-	if(runningDown == 0) {
-		runningDown = 1;
-		decrement(document.getElementById("inputTime").value);
-		document.getElementById("playDown").className = "fa fa-pause";
-	}
+	startPauseUp();
+	startPauseDown();
 	
 }
 function startPauseUp() {
@@ -33,7 +26,8 @@ function startPauseUp() {
 function startPauseDown() {
 	if(runningDown == 0) {
 		runningDown = 1;
-		decrement();
+		console.log('TImedown outside decrement ' + timeDown);
+		decrement(timeDown);
 		document.getElementById("playDown").className = "fa fa-pause";
 	}
 	else {
@@ -55,6 +49,7 @@ function resetUp() {
 
 }
 function resetDown() {
+	clearTimeout(resetDownId);
 	runningDown = 0;
 	timeDown = 0;
 	document.getElementById("getDaysDown").innerHTML = "00";
@@ -63,51 +58,52 @@ function resetDown() {
 	document.getElementById("getSecDown").innerHTML = "00";
 	document.getElementById("playDown").className = "fa fa-play";
 }
-function increment () {
-	if(runningUp == 1) {
-		targetUp = getTarget();
-		setTimeout(function(){
-			if(targetUp >= timeUp ) {
-			var days  =  Math.floor(timeUp / 86400);
-			var hours =  Math.floor((timeUp % 86400) / 3600);
-			var mins = Math.floor(((timeUp % 86400) % 3600) / 60);
-			var secs =  Math.floor(((timeUp % 86400) % 3600) % 60);;
+// function increment () {
+// 	if(runningUp == 1) {
+// 		targetUp = getTarget();
+// 		setTimeout(function(){
+// 			if(targetUp >= timeUp ) {
+// 			var days  =  Math.floor(timeUp / 86400);
+// 			var hours =  Math.floor((timeUp % 86400) / 3600);
+// 			var mins = Math.floor(((timeUp % 86400) % 3600) / 60);
+// 			var secs =  Math.floor(((timeUp % 86400) % 3600) % 60);;
 			
-			timeUp++;
+// 			timeUp++;
 
-			if (days < 10) {days = "0" + days;}
-			if (hours < 10) {hours = "0" + hours;}
-			if (mins < 10) {mins = "0" + mins;}
-			if (secs < 10) {secs = "0" + secs;}
+// 			if (days < 10) {days = "0" + days;}
+// 			if (hours < 10) {hours = "0" + hours;}
+// 			if (mins < 10) {mins = "0" + mins;}
+// 			if (secs < 10) {secs = "0" + secs;}
 
-			document.getElementById("getDaysUp").innerHTML = days;
-			document.getElementById("getHrUp").innerHTML = hours;
-			document.getElementById("getMinUp").innerHTML = mins;
-			document.getElementById("getSecUp").innerHTML = secs;
-			increment(); }
-			else {
-				runningUp = 0;
-				timeUp = 0;
-				document.getElementById("playUp").className = "fa fa-play";
-			}
-		}, 1000);
-	}
-}
-// function decrement () {
-// 	console.log('decrement() called');
+// 			document.getElementById("getDaysUp").innerHTML = days;
+// 			document.getElementById("getHrUp").innerHTML = hours;
+// 			document.getElementById("getMinUp").innerHTML = mins;
+// 			document.getElementById("getSecUp").innerHTML = secs;
+// 			increment(); }
+// 			else {
+// 				runningUp = 0;
+// 				timeUp = 0;
+// 				document.getElementById("playUp").className = "fa fa-play";
+// 			}
+// 		}, 1000);
+// 	}
 // }
+function increment () {
+	console.log('increment() called');
+}
 var count = 0;
 function decrement (timeDown) {
 	if(runningDown == 1) {
-		setTimeout(function(){ 
+		resetDownId = setTimeout(function(){ 
 			if(timeDown >= 0 ) {
 			var days  =  Math.floor(timeDown / 86400);
 			var hours =  Math.floor((timeDown % 86400) / 3600);
 			var mins = Math.floor(((timeDown % 86400) % 3600) / 60);
-			var secs =  Math.floor(((timeDown % 86400) % 3600) % 60);;
+			var secs =  Math.floor(((timeDown % 86400) % 3600) % 60);
 			count++;
-			timeDown--;
 
+			timeDown--;
+			console.log('TImedown inside decrement ' + timeDown);
 			if (days < 10) {days = "0" + days;}
 			if (hours < 10) {hours = "0" + hours;}
 			if (mins < 10) {mins = "0" + mins;}
